bc673064bd81bce28f2e40eb8a5e7edcb349b84c
Andres Mauricio Morales Ortega <Andres.Morales@coxautoinc.com>
2025-07-16 15:12:28 -0500

    Update removing comments

--- src/main/java/co/com/arrive/repository/InvoiceRepository.java
+++ src/main/java/co/com/arrive/repository/InvoiceRepository.java
@@ -9,7 +9,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
 
     Page<Invoice> findAllByDeletedAtIsNull(Pageable pageable);
-
-    //@Query("SELECT DISTINCT b FROM Book b WHERE b.library.id = :libraryId AND b.status = :status")
-    // Page<Book> findByLibraryIdAndStatus(Long libraryId, Book.BookStatus status, Pageable pageable);
 }
